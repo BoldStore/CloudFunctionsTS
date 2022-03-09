9eed19e88fa889a9ea437a14e376d940259f0cb7
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-03-10 01:53:08 +0530

    Product model

---
+++ functions/src/models/product.ts
@@ -0,0 +1,49 @@
+class Product {
+  name: string;
+  size: string;
+  sold: string;
+  postedOn: Date;
+  amount: string;
+  likes: string;
+  comments: string;
+  store: string;
+  color?: string;
+  soldOn?: Date;
+
+  constructor(
+    name: string,
+    size: string,
+    sold: string,
+    postedOn: Date,
+    amount: string,
+    likes: string,
+    comments: string,
+    store: string,
+    color?: string,
+    soldOn?: Date
+  ) {
+    this.name = name;
+    this.size = size;
+    this.sold = sold;
+    this.amount = amount;
+    this.likes = likes;
+    this.comments = comments;
+    this.store = store;
+    this.color = color;
+    this.postedOn = postedOn;
+    this.soldOn = soldOn;
+
+    return {
+      name: name,
+      size: size,
+      sold: sold,
+      amount: amount,
+      likes: likes,
+      comments: comments,
+      store: store,
+      color: color,
+      postedOn: postedOn,
+      soldOn: soldOn,
+    };
+  }
+}
