3268b3e905948c492bd82c3a19aaa91966d7baf4
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-04-05 22:02:15 +0530

    Bug

--- functions/src/codes.ts
+++ functions/src/codes.ts
@@ -1,6 +1,6 @@
 import { auth, firestore } from "firebase-admin";
 import { https, Request, Response } from "firebase-functions/v1";
-import { generateCode } from "./helper/generateCode";
+import { generateCode } from "./helper/code";
 
 exports.addInviteToken = https.onRequest(
   async (req: Request, res: Response<any>) => {
