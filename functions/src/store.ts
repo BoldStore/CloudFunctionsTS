992cdb2e12e7c47e435d5fbad505886b0ed3f83d
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-04-09 09:21:41 +0530

    create store

--- functions/src/store.ts
+++ functions/src/store.ts
@@ -2,6 +2,45 @@ import { https, Request, Response } from "firebase-functions/v1";
 import { firestore, auth } from "firebase-admin";
 import { refresh_store_data } from "./helper/store";
 
+exports.createStore = https.onRequest(
+  async (req: Request, res: Response<any>) => {
+    const token = req.headers.authorization!;
+    const id: string = (await auth().verifyIdToken(token)).uid;
+    const email = req.body.email;
+
+    if (!id) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    const user = await firestore().collection("users").doc(id).get();
+    const store = await firestore().collection("stores").doc(id).get();
+
+    if (user.exists) {
+      res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+      return;
+    }
+
+    if (store.exists) {
+      res.status(400).json({
+        success: false,
+        message: "Store already exists",
+      });
+      return;
+    }
+
+    await firestore().collection("stores").doc(id).set({
+      email,
+    });
+  }
+);
+
 exports.updateStore = https.onRequest(
   async (req: Request, res: Response<any>) => {
     const token = req.headers.authorization!;
