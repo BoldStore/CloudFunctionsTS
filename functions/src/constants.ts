c5d0d4d79526b67c7bd476356d88ef8c5663506b
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-01 15:03:02 +0530

    Fix shipping

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -7,7 +7,7 @@ export const BASIC_FIELDS = "id,username";
 export const MEDIA_FIELDS =
   "caption,id,media_type,media_url,permalink,timestamp";
 
-export const SHIPROCKET_API_URL = "https://apiv2.shiprocket.in/v1/";
+export const SHIPROCKET_API_URL = "https://apiv2.shiprocket.in/v1";
 export const SHIPROCKET_SERVICEABILITY =
   SHIPROCKET_API_URL + "/external/courier/serviceability/";
 export const SHIPROCKET_LOGIN = SHIPROCKET_API_URL + "/external/auth/login";
