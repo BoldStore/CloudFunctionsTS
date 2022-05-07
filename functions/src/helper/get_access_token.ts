07c57423097139d12473cde090e977ba75fff340
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-07 10:25:51 +0530

    Almost get userid

--- functions/src/helper/get_access_token.ts
+++ functions/src/helper/get_access_token.ts
@@ -23,8 +23,17 @@ export const getAccessToken = async (code?: string) => {
 
   if (response.status === 200) {
     access_token = response.data.access_token;
-    user_id = response.data.user_id;
+    let user_id_res: string = response.data.user_id.toString();
+    user_id_res = user_id_res.replace(
+      user_id_res.slice(-1),
+      (parseInt(user_id_res.slice(-1), 10) + 1).toString()
+    );
+    console.log("RES>>", user_id_res);
+    user_id = user_id_res;
+    // user_id = (parseInt(response.data.user_id) + 1).toString();
   }
+
+  console.log("USERIDDD", user_id);
 
   return {
     access_token,
