3c99095d3923827d24b4706f2269d8d1b6ca89b6
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-10 15:54:58 +0530

    Addresses done

--- functions/src/address.ts
+++ functions/src/address.ts
@@ -4,44 +4,31 @@ import { checkAuth } from "./helper/check_auth";
 
 exports.addAddress = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const userId = (await checkAuth(req, res))!;
+    try {
+      const userId = (await checkAuth(req, res))!;
 
-    const title = req.body.title;
-    const address_string = req.body.address;
-    const addressL1 = req.body.addressL1;
-    const addressL2 = req.body.addressL2;
-    const city = req.body.city;
-    const state = req.body.state;
-    const pincode = req.body.pincode;
-    const notes = req.body.notes;
+      const title = req.body.title;
+      const address_string = req.body.address;
+      const addressL1 = req.body.addressL1;
+      const addressL2 = req.body.addressL2;
+      const city = req.body.city;
+      const state = req.body.state;
+      const pincode = req.body.pincode;
+      const notes = req.body.notes;
 
-    // const address_model = new Address(
-    //   address_string,
-    //   title,
-    //   addressL1,
-    //   addressL2,
-    //   city,
-    //   state,
-    //   pincode,
-    //   userId,
-    //   notes
-    // );
+      // const address_model = new Address(
+      //   address_string,
+      //   title,
+      //   addressL1,
+      //   addressL2,
+      //   city,
+      //   state,
+      //   pincode,
+      //   userId,
+      //   notes
+      // );
 
-    const address = await firestoredb().collection("addresses").add({
-      address_string,
-      title,
-      addressL1,
-      addressL2,
-      city,
-      state,
-      pincode,
-      userId,
-      notes,
-    });
-
-    res.status(201).json({
-      success: true,
-      address: {
+      const address = await firestoredb().collection("addresses").add({
         address_string,
         title,
         addressL1,
@@ -51,105 +38,154 @@ exports.addAddress = https.onRequest(
         pincode,
         userId,
         notes,
-        id: address.id,
-      },
-    });
+      });
+
+      res.status(201).json({
+        success: true,
+        address: {
+          address_string,
+          title,
+          addressL1,
+          addressL2,
+          city,
+          state,
+          pincode,
+          userId,
+          notes,
+          id: address.id,
+        },
+      });
+    } catch (e) {
+      console.log("Add address error: ", e);
+      res.status(500).json({
+        success: false,
+        message: "Could not add address",
+        error: e,
+      });
+    }
   }
 );
 
 exports.getUserAddresses = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const userId = (await checkAuth(req, res))!;
+    try {
+      const userId = (await checkAuth(req, res))!;
 
-    const addresses = await firestoredb()
-      .collection("addresses")
-      .where("userId", "==", userId)
-      .get();
+      const addresses = await firestoredb()
+        .collection("addresses")
+        .where("userId", "==", userId)
+        .get();
 
-    res.status(200).json({
-      success: true,
-      addresses: addresses.docs.map((address) => ({
-        id: address.id,
-        ...address.data(),
-      })),
-    });
+      res.status(200).json({
+        success: true,
+        addresses: addresses.docs.map((address) => ({
+          id: address.id,
+          ...address.data(),
+        })),
+      });
+    } catch (e) {
+      console.log("Get user addresses error: ", e);
+      res.status(500).json({
+        success: false,
+        message: "Could not get user addresses",
+        error: e,
+      });
+    }
   }
 );
 
 exports.updateAddress = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id: string = req.body.id!.toString();
-    const userId = (await checkAuth(req, res))!;
+    try {
+      const id: string = req.body.id!.toString();
+      const userId = (await checkAuth(req, res))!;
 
-    const title = req.body.title;
-    const address_string = req.body.address;
-    const addressL1 = req.body.addressL1;
-    const addressL2 = req.body.addressL2;
-    const city = req.body.city;
-    const state = req.body.state;
-    const pincode = req.body.pincode;
-    const notes = req.body.notes;
+      const title = req.body.title;
+      const address_string = req.body.address;
+      const addressL1 = req.body.addressL1;
+      const addressL2 = req.body.addressL2;
+      const city = req.body.city;
+      const state = req.body.state;
+      const pincode = req.body.pincode;
+      const notes = req.body.notes;
 
-    // const address_model = new Address(
-    //   address_string,
-    //   title,
-    //   addressL1,
-    //   addressL2,
-    //   city,
-    //   state,
-    //   pincode,
-    //   userId,
-    //   notes
-    // );
+      // const address_model = new Address(
+      //   address_string,
+      //   title,
+      //   addressL1,
+      //   addressL2,
+      //   city,
+      //   state,
+      //   pincode,
+      //   userId,
+      //   notes
+      // );
 
-    await firestoredb().collection("addresses").doc(id).set(
-      {
-        address_string,
-        title,
-        addressL1,
-        addressL2,
-        city,
-        state,
-        pincode,
-        userId,
-        notes,
-      },
-      { merge: true }
-    );
+      await firestoredb().collection("addresses").doc(id).set(
+        {
+          address_string,
+          title,
+          addressL1,
+          addressL2,
+          city,
+          state,
+          pincode,
+          userId,
+          notes,
+        },
+        { merge: true }
+      );
 
-    const addresses = await firestoredb()
-      .collection("addresses")
-      .where("userId", "==", userId)
-      .get();
+      const addresses = await firestoredb()
+        .collection("addresses")
+        .where("userId", "==", userId)
+        .get();
 
-    res.status(200).json({
-      success: true,
-      addresses: addresses.docs.map((address) => ({
-        id: address.id,
-        ...address.data(),
-      })),
-    });
+      res.status(200).json({
+        success: true,
+        addresses: addresses.docs.map((address) => ({
+          id: address.id,
+          ...address.data(),
+        })),
+      });
+    } catch (e) {
+      console.log("Update address error: ", e);
+      res.status(500).json({
+        success: false,
+        message: "Could not update address",
+        error: e,
+      });
+    }
   }
 );
 
 exports.deleteAddress = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id: string = req.body.id!.toString();
-    const userId = (await checkAuth(req, res))!;
+    try {
+      const id: string = req.body.id!.toString();
+      const userId = (await checkAuth(req, res))!;
 
-    await firestoredb().collection("addresses").doc(id).delete();
+      await firestoredb().collection("addresses").doc(id).delete();
 
-    const addresses = await firestoredb()
-      .collection("addresses")
-      .where("userId", "==", userId)
-      .get();
+      const addresses = await firestoredb()
+        .collection("addresses")
+        .where("userId", "==", userId)
+        .get();
 
-    res.status(200).json({
-      success: true,
-      addresses: addresses.docs.map((address) => ({
-        id: address.id,
-        ...address.data(),
-      })),
-    });
+      res.status(200).json({
+        success: true,
+        addresses: addresses.docs.map((address) => ({
+          id: address.id,
+          ...address.data(),
+        })),
+      });
+    } catch (e) {
+      console.log("Delete address error: ", e);
+      res.status(500).json({
+        success: false,
+        message: "Could not delete address",
+        error: e,
+      });
+    }
   }
 );
