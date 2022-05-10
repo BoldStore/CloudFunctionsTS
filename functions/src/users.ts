02d07de2b3f4638872d3e05de0f89e056a2a30e9
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-10 22:14:10 +0530

    User done

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -20,110 +20,183 @@ exports.createUser = auth.user().onCreate(async (user) => {
 });
 
 exports.userToDb = https.onRequest(async (req: Request, res: Response<any>) => {
-  const id = (await checkAuth(req, res))!.userId!;
-  const email = req.body.email;
+  try {
+    const authData = await checkAuth(req, res);
+    const id = authData!.userId!;
+    const email = authData!.email;
 
-  const user = await firestoredb().collection("users").doc(id).get();
-  const store = await firestoredb().collection("stores").doc(id).get();
+    const user = await firestoredb().collection("users").doc(id).get();
+    const store = await firestoredb().collection("stores").doc(id).get();
 
-  if (user.exists) {
-    res.status(400).json({
-      success: false,
-      message: "User already exists",
+    if (user.exists) {
+      res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+      return;
+    }
+
+    if (store.exists) {
+      res.status(400).json({
+        success: false,
+        message: "Store already exists",
+      });
+      return;
+    }
+
+    await firestoredb().collection("users").doc(id).set({
+      email,
     });
-    return;
-  }
 
-  if (store.exists) {
-    res.status(400).json({
-      success: false,
-      message: "Store already exists",
+    res.status(201).json({
+      success: true,
+      user: {
+        email: email,
+      },
     });
-    return;
+  } catch (e) {
+    console.log("Error in adding user", e);
+    res.status(500).json({
+      success: false,
+      message: "Error in adding user",
+      error: e,
+    });
   }
-
-  await firestoredb().collection("users").doc(id).set({
-    email,
-  });
-
-  res.status(201).json({
-    success: true,
-  });
 });
 
 exports.addInstaUsername = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id = (await checkAuth(req, res))!.userId!;
+    try {
+      const id = (await checkAuth(req, res))!.userId!;
 
-    const insta_username = req.body.insta_username!.toString();
+      const insta_username = req.body.insta_username!.toString();
 
-    const data = await getInstaData(insta_username);
+      // Check if already exists
+      const userDb = (
+        await firestoredb()
+          .collection("users")
+          .where("insta_username", "==", insta_username)
+          .limit(1)
+          .get()
+      ).docs;
 
-    const user = await firestoredb().collection("users").doc(id).set(
-      {
-        insta_username: insta_username,
-        name: data.full_name,
-        imgUrl: data.profile_pic,
-      },
-      { merge: true }
-    );
+      if (userDb.length > 0) {
+        res.status(400).json({
+          success: false,
+          message: "Instagram username already exists",
+        });
+        return;
+      }
 
-    res.status(200).json({
-      success: true,
-      user,
-    });
+      const data = await getInstaData(insta_username);
+
+      const user = await firestoredb().collection("users").doc(id).set(
+        {
+          insta_username: insta_username,
+          name: data.full_name,
+          imgUrl: data.profile_pic,
+        },
+        { merge: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        user: {
+          insta_username: insta_username,
+          name: data.full_name,
+          imgUrl: data.profile_pic,
+        },
+        userResult: user,
+      });
+    } catch (e) {
+      console.log("Error in adding insta username", e);
+      res.status(500).json({
+        success: false,
+        message: "Error in adding insta username",
+        error: e,
+      });
+    }
   }
 );
 
 exports.getPersonalDetails = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id = (await checkAuth(req, res))!.userId!;
+    try {
+      const id = (await checkAuth(req, res))!.userId!;
 
-    const user = (await firestoredb().collection("users").doc(id).get()).data();
+      const user = (
+        await firestoredb().collection("users").doc(id).get()
+      ).data();
 
-    res.status(200).json({
-      success: true,
-      user,
-    });
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (e) {
+      console.log("Error in getting personal details", e);
+      res.status(500).json({
+        success: false,
+        message: "Error in getting personal details",
+        error: e,
+      });
+    }
   }
 );
 
 exports.updateUser = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id = (await checkAuth(req, res))!.userId!;
+    try {
+      const id = (await checkAuth(req, res))!.userId!;
 
-    const name = req.body.name;
-    const birthday = req.body.birthday;
-    const sizePreference = req.body.sizePreference;
-    // const insta_username = req.body.insta_username;
-    const phone = req.body.phone;
+      const name = req.body.name;
+      const birthday = req.body.birthday;
+      const sizePreference = req.body.sizePreference;
+      // const insta_username = req.body.insta_username;
+      const phone = req.body.phone;
 
-    const user = await firestoredb().collection("users").doc(id).update({
-      name,
-      birthday,
-      sizePreference,
-      // insta_username,
-      phone,
-    });
+      const user = await firestoredb().collection("users").doc(id).update({
+        name,
+        birthday,
+        sizePreference,
+        // insta_username,
+        phone,
+      });
 
-    res.status(200).json({
-      success: true,
-      user,
-    });
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (e) {
+      console.log("Error in updating user", e);
+      res.status(500).json({
+        success: false,
+        message: "Error in updating user",
+        error: e,
+      });
+    }
   }
 );
 
 exports.deleteUser = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const id = (await checkAuth(req, res))!.userId!;
+    try {
+      const id = (await checkAuth(req, res))!.userId!;
 
-    const user = await firestoredb().collection("users").doc(id).update({
-      deletedOn: new Date(),
-    });
+      const user = await firestoredb().collection("users").doc(id).update({
+        deletedOn: new Date(),
+      });
 
-    res.status(200).json({
-      success: true,
-      user,
-    });
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (e) {
+      console.log("Error in deleting user", e);
+      res.status(500).json({
+        success: false,
+        message: "Error in deleting user",
+        error: e,
+      });
+    }
   }
 );
