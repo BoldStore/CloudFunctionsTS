67fdc1aa350875f7ef3a0a5339125595851514b1
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-10 15:58:57 +0530

    Invite token done

--- functions/src/codes.ts
+++ functions/src/codes.ts
@@ -5,57 +5,66 @@ import { generateCode } from "./helper/code";
 
 exports.addInviteToken = https.onRequest(
   async (req: Request, res: Response<any>) => {
-    const userId = (await checkAuth(req, res))!;
+    try {
+      const userId = (await checkAuth(req, res))!;
 
-    const store = await firestore().collection("stores").doc(userId).get();
+      const store = await firestore().collection("stores").doc(userId).get();
 
-    if (!store.exists) {
-      res.status(404).json({
-        success: false,
-        message: "Store not found",
-      });
-      return;
-    }
+      if (!store.exists) {
+        res.status(404).json({
+          success: false,
+          message: "Store not found",
+        });
+        return;
+      }
 
-    const today = new Date();
-    const yesterday = new Date();
-    yesterday.setDate(today.getDate() - 1);
+      const today = new Date();
+      const yesterday = new Date();
+      yesterday.setDate(today.getDate() - 1);
 
-    const tokensByStore = await firestore()
-      .collection("inviteTokens")
-      .where("storeId", "==", userId)
-      .where("createdAt", "<=", today)
-      .where("createdAt", ">=", yesterday)
-      .get();
+      const tokensByStore = await firestore()
+        .collection("inviteTokens")
+        .where("storeId", "==", userId)
+        .where("createdAt", "<=", today)
+        .where("createdAt", ">=", yesterday)
+        .get();
 
-    if (tokensByStore.docs.length >= 3) {
-      res.status(400).json({
-        success: false,
-        message: "You have already generated 3 invite tokens today",
-      });
-      return;
-    }
+      if (tokensByStore.docs.length >= 3) {
+        res.status(400).json({
+          success: false,
+          message: "You have already generated 3 invite tokens today",
+        });
+        return;
+      }
 
-    const inviteCode = await generateCode();
+      const inviteCode = await generateCode();
 
-    // const code = new Code(inviteCode, userId, new Date(), true);
+      // const code = new Code(inviteCode, userId, new Date(), true);
 
-    // Add to db
-    await firestore().collection("codes").add({
-      code: inviteCode,
-      createdBy: userId,
-      createdAt: new Date(),
-      isActive: true,
-    });
-
-    res.status(201).json({
-      success: true,
-      code: {
+      // Add to db
+      await firestore().collection("codes").add({
         code: inviteCode,
         createdBy: userId,
         createdAt: new Date(),
         isActive: true,
-      },
-    });
+      });
+
+      res.status(201).json({
+        success: true,
+        code: {
+          code: inviteCode,
+          createdBy: userId,
+          createdAt: new Date(),
+          isActive: true,
+        },
+      });
+    } catch (e) {
+      console.log("Add invite token error: ", e);
+      res.status(500).json({
+        success: false,
+        message: "Error adding invite token",
+        error: e,
+      });
+    }
   }
 );
