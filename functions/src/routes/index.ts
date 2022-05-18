31cd7c746e63b72b363d618712fbeb14bc39d9af
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-18 16:22:58 +0530

    Add to index

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import * as addressRoutes from "./address";
 import * as codeRoutes from "./code";
+import * as pageRoutes from "./pages";
+import * as storeRoutes from "./store";
 import * as testRoutes from "../test";
 // eslint-disable-next-line new-cap
 const router = Router();
 
 router.use("/address", addressRoutes);
 router.use("/codes", codeRoutes);
+router.use("/pages", pageRoutes);
+router.use("/store", storeRoutes);
 router.use("/test", testRoutes);
 
 export = router;
