cc6f2357a3ed62c67b65e37551d8a59412facf74
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-06 21:16:37 +0530

    Tasks warnings

--- functions/src/tasks/products.ts
+++ functions/src/tasks/products.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { v2beta3 } from "@google-cloud/tasks";
-// import { firestore } from "firebase-admin";
 import {
   GCP_LOCATION,
   GCP_PROJECT_NAME,
@@ -7,17 +7,17 @@ import {
   SERVICE_ACCOUNT_EMAIL,
 } from "../secrets";
 
-export const createProductTask = async (
-  posts: Array<any> = [],
+export const createProductTask: (
+  posts: Array<any>,
   storeId: string
-) => {
+) => Promise<any> = async (posts = [], storeId) => {
   const client = new v2beta3.CloudTasksClient();
 
   const queueName = "product-queue";
 
   const parent = client.queuePath(
-    GCP_PROJECT_NAME!.toString(),
-    GCP_LOCATION!.toString(),
+    GCP_PROJECT_NAME.toString(),
+    GCP_LOCATION.toString(),
     queueName
   );
 
@@ -29,22 +29,16 @@ export const createProductTask = async (
   const convertedPayload = JSON.stringify(payload);
   const body = Buffer.from(convertedPayload).toString("base64");
 
-  // const store = await firestore().collection("stores").doc(storeId).get();
-
   let url = "";
 
-  // if (!store.exists) {
-  //   url = PRODUCT_DATA_URL!.toString() + "?storeId=" + storeId;
-  // } else {
-  // }
-  url = PRODUCT_DATA_URL!.toString() + "?storeId=" + storeId;
+  url = PRODUCT_DATA_URL.toString() + "?storeId=" + storeId;
 
   const task: any = {
     httpRequest: {
       httpMethod: "post",
       url,
       oidcToken: {
-        serviceAccountEmail: SERVICE_ACCOUNT_EMAIL!.toString(),
+        serviceAccountEmail: SERVICE_ACCOUNT_EMAIL.toString(),
         audience: new URL(url).origin,
       },
       headers: {
@@ -58,28 +52,22 @@ export const createProductTask = async (
   const convertedDate = new Date(date);
   const currentDate = new Date();
 
-  // Schedule time can not be in the past.
   if (convertedDate < currentDate) {
     console.error("Scheduled date in the past.");
   } else if (convertedDate > currentDate) {
     const date_diff_in_seconds =
       (convertedDate.valueOf() - currentDate.valueOf()) / 1000;
-    // Construct future date in Unix time.
     const date_in_seconds = date_diff_in_seconds + Date.now() / 1000;
-    // Add schedule time to request in Unix time using Timestamp structure.
-    // https://googleapis.dev/nodejs/tasks/latest/google.protobuf.html#.Timestamp
     task.scheduleTime = {
       seconds: date_in_seconds,
     };
   }
 
   try {
-    // Send create task request.
     const [response] = await client.createTask({ parent, task });
     console.log(`Created task ${response.name}`);
     return response.name;
   } catch (error) {
-    // Construct error for Stackdriver Error Reporting
     console.log("There was an error");
     console.error(error);
     return error;
