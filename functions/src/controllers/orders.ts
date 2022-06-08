170f8c023d4341dc5ec4a3ffb8542a6b556fb579
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-08 17:42:31 +0530

    Order model and create order

--- functions/src/controllers/orders.ts
+++ functions/src/controllers/orders.ts
@@ -1,10 +1,72 @@
 import { NextFunction, Request, Response } from "express";
+import { firestore } from "firebase-admin";
+import { razorpayInstance } from "..";
+import { Order, OrderType } from "../models/orders";
+import ExpressError = require("../utils/ExpressError");
 
 export const createOrder: (
   req: Request,
   res: Response,
   next: NextFunction
-) => Promise<void> = async (req, res, next) => {};
+) => Promise<void> = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const product_id: string = req.body.product_id;
+    const address_id: string = req.body.address_id;
+    const currency = "INR";
+    const id = user.uid;
+
+    const product = await firestore()
+      .collection("products")
+      .doc(product_id)
+      .get();
+
+    if (!product?.exists) {
+      next(new ExpressError("Product not found", 400));
+      return;
+    }
+
+    if (product.data() && !product.data()?.sold) {
+      next(new ExpressError("Product is sold out", 400));
+      return;
+    }
+
+    if (product.data() && product.data()?.available) {
+      next(new ExpressError("Product is not available", 400));
+      return;
+    }
+    // TODO: Check if product is available on insta
+
+    // Make the product unavailable
+    await firestore().collection("products").doc(product.id).update({
+      available: false,
+    });
+
+    // Add to razorpay
+    await razorpayInstance.orders
+      .create({ amount: product.data()?.price ?? 1000, currency: currency })
+      .then(async (order) => {
+        const order_obj: OrderType = new Order(
+          product.id,
+          product.data()?.price ?? 1000,
+          undefined,
+          order.id,
+          address_id,
+          currency,
+          product.data()?.store,
+          undefined,
+          new Date(),
+          id
+        );
+
+        await firestore().collection("orders").add(order_obj);
+        res.status(201).json({ success: true, order: order_obj });
+      });
+  } catch (e) {
+    console.log("Error in creating order", e);
+    next(new ExpressError("Internal server error", 500, e));
+  }
+};
 
 export const previousOrders: (
   req: Request,
--- functions/src/models/orders.ts
+++ functions/src/models/orders.ts
@@ -1,54 +1,147 @@
 /* eslint-disable require-jsdoc */
-class Order {
+export class Order {
   product: string;
   amount: string;
-  createdAt: Date;
-  confirmed: boolean;
+  status: string;
   orderId: string;
-  paymentId: string;
-  user: string;
   address: string;
   currency: string;
-  seller: string;
   store: string;
+  createdAt: Date;
+  paymentId?: string | null;
+  user?: string | null;
+  label_url?: string | null;
+  manifest_url?: string | null;
+  pickup_scheduled_date?: {
+    date: string;
+    timezone_type: string;
+    timezone: string;
+  } | null;
+  shiprocket_order_id?: string | null;
+  shipment_id?: string | null;
+  awb_code?: string | null;
+  courier_company_id?: number | null;
+  courier_name?: string | null;
+  assigned_date_time?: {
+    date: string;
+    timezone_type: string;
+    timezone: string;
+  } | null;
+  routing_code?: string | null;
+  pickup_token_number?: string | null;
+  applied_weight?: number | null;
 
   constructor(
     product: string,
     amount: string,
-    createdAt: Date,
-    confirmed: boolean,
+    status = "pending",
     orderId: string,
-    paymentId: string,
-    user: string,
     address: string,
     currency: string,
-    seller: string,
-    store: string
+    store: string,
+    paymentId: string | null = null,
+    createdAt: Date = new Date(),
+    user: string | null = null,
+    label_url: string | null = null,
+    manifest_url: string | null = null,
+    pickup_scheduled_date: {
+      date: string;
+      timezone_type: string;
+      timezone: string;
+    } | null = null,
+    shiprocket_order_id: string | null = null,
+    shipment_id: string | null = null,
+    awb_code: string | null = null,
+    courier_company_id: number | null = null,
+    courier_name: string | null = null,
+    assigned_date_time: {
+      date: string;
+      timezone_type: string;
+      timezone: string;
+    } | null = null,
+    routing_code: string | null = null,
+    pickup_token_number: string | null = null,
+    applied_weight: number | null = null
   ) {
     this.product = product;
     this.amount = amount;
-    this.createdAt = createdAt;
-    this.confirmed = confirmed;
+    this.status = status;
     this.orderId = orderId;
-    this.paymentId = paymentId;
-    this.user = user;
     this.address = address;
     this.currency = currency;
-    this.seller = seller;
     this.store = store;
+    this.createdAt = createdAt;
+    this.paymentId = paymentId;
+    this.user = user;
+    this.label_url = label_url;
+    this.manifest_url = manifest_url;
+    this.pickup_scheduled_date = pickup_scheduled_date;
+    this.shiprocket_order_id = shiprocket_order_id;
+    this.shipment_id = shipment_id;
+    this.awb_code = awb_code;
+    this.courier_company_id = courier_company_id;
+    this.courier_name = courier_name;
+    this.assigned_date_time = assigned_date_time;
+    this.routing_code = routing_code;
+    this.pickup_token_number = pickup_token_number;
+    this.applied_weight = applied_weight;
 
     return {
       product: this.product,
       amount: this.amount,
-      createdAt: this.createdAt,
-      confirmed: this.confirmed,
+      status: this.status,
       orderId: this.orderId,
-      paymentId: this.paymentId,
-      user: this.user,
       address: this.address,
       currency: this.currency,
-      seller: this.seller,
       store: this.store,
+      createdAt: this.createdAt,
+      paymentId: this.paymentId,
+      user: this.user,
+      label_url: this.label_url,
+      manifest_url: this.manifest_url,
+      pickup_scheduled_date: this.pickup_scheduled_date,
+      shiprocket_order_id: this.shiprocket_order_id,
+      shipment_id: this.shipment_id,
+      awb_code: this.awb_code,
+      courier_company_id: this.courier_company_id,
+      courier_name: this.courier_name,
+      assigned_date_time: this.assigned_date_time,
+      routing_code: this.routing_code,
+      pickup_token_number: this.pickup_token_number,
+      applied_weight: this.applied_weight,
     };
   }
+}
+
+export interface OrderType {
+  product: string;
+  amount: string;
+  status: string;
+  orderId: string;
+  address: string;
+  currency: string;
+  store: string;
+  createdAt: Date;
+  paymentId?: string | null;
+  user?: string | null;
+  label_url?: string | null;
+  manifest_url?: string | null;
+  pickup_scheduled_date?: {
+    date: string;
+    timezone_type: string;
+    timezone: string;
+  } | null;
+  shiprocket_order_id?: string | null;
+  shipment_id?: string | null;
+  awb_code?: string | null;
+  courier_company_id?: number | null;
+  courier_name?: string | null;
+  assigned_date_time?: {
+    date: string;
+    timezone_type: string;
+    timezone: string;
+  } | null;
+  routing_code?: string | null;
+  pickup_token_number?: string | null;
+  applied_weight?: number | null;
 }
