7ebd63d8debb5dd235aea7dec57bcf7703cc5d83
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-11 01:57:31 +0530

    Add todo

--- functions/src/helper/insta/get_store_data.ts
+++ functions/src/helper/insta/get_store_data.ts
@@ -14,6 +14,7 @@ import { deleteObject, handler } from "../s3/file_upload_s3";
 import { getInstaData } from "./get_insta_data";
 
 // TODO: Add a check in store if all products fetched
+// TODO: Get store in post, think of an optimal solution
 
 export const getStoreData: (
   user_id: string,
