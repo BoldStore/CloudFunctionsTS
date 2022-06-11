19f430aa9693080f7350b8a6851b76b872031077
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-11 16:28:54 +0530

    Add products fetch check

--- functions/src/controllers/product.ts
+++ functions/src/controllers/product.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { firestore } from "firebase-admin";
 import { addProduct } from "../helper/product";
 import ExpressError = require("../utils/ExpressError");
 
@@ -16,10 +17,20 @@ export const getProductData: (
 
     const posts = req.body.posts;
 
+    // Get store and add a post status
+    await firestore().collection("stores").doc(storeId).update({
+      postsStatus: "fetching",
+    });
+
     for (let i = 0; i < posts?.length; i++) {
       const post = posts[i];
       await addProduct(storeId, post);
     }
+
+    // Update the post status to completed
+    await firestore().collection("stores").doc(storeId).update({
+      postsStatus: "completed",
+    });
 
     res.status(200).json({
       success: true,
