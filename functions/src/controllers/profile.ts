f34d26bb116010973752203c436d5a8f1b080b7d
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-24 00:18:18 +0530

    smol ubg

--- functions/src/controllers/profile.ts
+++ functions/src/controllers/profile.ts
@@ -44,7 +44,7 @@ export const getProfile: (
         data: user.data(),
         isStore: false,
         percentage,
-        address: { ...address?.data(), id: address.id },
+        address: { ...address?.data(), id: address?.id },
         name,
         phone,
       });
@@ -80,7 +80,7 @@ export const getProfile: (
         success: true,
         data: store?.data(),
         isStore: true,
-        address: { ...address?.data(), id: address.id },
+        address: { ...address?.data(), id: address?.id },
         paymentDetails: paymentDetails?.data(),
         percentage,
       });
